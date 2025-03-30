9f12aefade0786b381ee351d355d13b9296fe91d
Beatriz Ribeiro <biiaribeiro60@gmail.com>
2025-03-30 18:47:01 -0300

    feat: Database configuration and queries.

---
+++ src/database/queries/queries.ts
@@ -0,0 +1,25 @@
+export const createTable = `
+  CREATE TABLE users (
+      id BIGINT AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL UNIQUE,
+      phone VARCHAR(50) NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );
+`
+export const addUserQuery = `
+  INSERT INTO users (name, email, phone)
+  VALUES (?, ?, ?)
+`;
+
+export const getAllUsersQuery = 'SELECT * FROM users ORDER BY name ASC';
+
+export const getUserByIdQuery = 'SELECT * FROM users WHERE id = ?';
+
+export const updateUserQuery = `
+  UPDATE users
+  SET name = ?, email = ?, phone = ?
+  WHERE id = ?
+`;
+
+export const deleteUserQuery = 'DELETE FROM users WHERE id = ?';
